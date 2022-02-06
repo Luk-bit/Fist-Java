73b78d9daaec0ca3a4412ba3bb1dc3dcfeea1d1b
Luk Francelino <lukcostavga@gmail.com>
2022-02-06 11:40:46 -0300

    Commit Intellij

---
+++ src/br/com/let/First.java
@@ -0,0 +1,14 @@
+package br.com.let;
+
+import br.com.let.model.gato;
+
+public class First {
+    public static void main(String[] args){
+        gato gato = new gato();
+
+        System.out.println(gato);
+      /*  int a = 4;
+        int b = 8;
+        System.out.println("Olá Mundo " + (a+b));*/
+    }
+}
